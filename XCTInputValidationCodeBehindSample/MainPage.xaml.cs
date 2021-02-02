99ed0bb9bc7e40ae4184d6c922827ff4cbaa2bec
Gerald Versluis <gerald@verslu.is>
2021-02-02 10:09:58 +0100

    Added Validation

--- XCTInputValidationCodeBehindSample/MainPage.xaml.cs
+++ XCTInputValidationCodeBehindSample/MainPage.xaml.cs
@@ -1,18 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 using Xamarin.Forms;
 
 namespace XCTInputValidationCodeBehindSample
 {
     public partial class MainPage : ContentPage
     {
+        private bool emailValid;
+
+        public bool EmailValid
+        {
+            get => emailValid;
+            set
+            {
+                emailValid = value;
+                OnPropertyChanged();
+            }
+        }
+
         public MainPage()
         {
             InitializeComponent();
+
+            BindingContext = this;
+        }
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            if (!myValidation.IsValid)
+            {
+                var errorBuilder = new StringBuilder();
+
+                foreach (var error in myValidation.Errors)
+                {
+                    if (error is string)
+                    {
+                        errorBuilder.AppendLine(error.ToString());
+                    }
+                }
+
+                DisplayAlert("Error", errorBuilder.ToString(), "OK");
+            }
+            else
+            {
+                DisplayAlert("Success", "All valid!", "OK");
+            }
+        }
+
+        void Button_Clicked_1(System.Object sender, System.EventArgs e)
+        {
+            if (!myEmailValidation.IsValid)
+            {
+                DisplayAlert("Error", "Email not valid", "OK");
+            }
+            else
+            {
+                DisplayAlert("Success", "All valid!", "OK");
+            }
         }
     }
 }
