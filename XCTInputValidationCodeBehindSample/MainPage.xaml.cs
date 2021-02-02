f8c7e29ae34e569fa8c9fe5d9dd90dae2129f02f
Gerald Versluis <gerald@verslu.is>
2021-02-02 21:25:29 +0100

    Implemented Validations

--- XCTInputValidationCodeBehindSample/MainPage.xaml.cs
+++ XCTInputValidationCodeBehindSample/MainPage.xaml.cs
@@ -10,9 +10,59 @@ namespace XCTInputValidationCodeBehindSample
 {
     public partial class MainPage : ContentPage
     {
+        private bool emailValid;
+
+        public bool EmailValid
+        {
+            get => emailValid;
+            set
+            {
+                emailValid = value;
+                OnPropertyChanged();
+            }
+        }
+
         public MainPage()
         {
             InitializeComponent();
+
+            BindingContext = this;
+        }
+
+        // Multi validation
+        async void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            if (!myMultiValidation.IsValid)
+            {
+                var errorBuilder = new StringBuilder();
+
+                foreach (var error in myMultiValidation.Errors)
+                {
+                    if (error is string)
+                    {
+                        errorBuilder.AppendLine(((string)error).ToString());
+                    }
+
+                    await DisplayAlert("Error", errorBuilder.ToString(), "OK");
+                }
+            }
+            else
+            {
+                await DisplayAlert("Hooray", "All valid", "OK");
+            }
+        }
+
+        // Email validation
+        async void Button_Clicked_1(System.Object sender, System.EventArgs e)
+        {
+            if (!myEmailValidation.IsValid)
+            {
+                await DisplayAlert("Error", "Email not valid", "OK");
+            }
+            else
+            {
+                await DisplayAlert("Hooray", "All valid", "OK");
+            }
         }
     }
 }
